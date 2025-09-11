2722d4143f9f1f5bf2b06c6bff51a65a20c5d2ce
Katie Bridson <kbridson@memphis.edu>
2025-09-10 19:14:06 -0500

    Added priority attribute to Task

--- task.py
+++ task.py
@@ -1,12 +1,13 @@
 class Task:
-    def __init__(self, title, description=""):
+    def __init__(self, title, description="", priority="Medium"):
         self.title = title
         self.description = description
+        self.priority = priority
         self.completed = False
-    
+
     def complete(self):
         self.completed = True
-    
+
     def __str__(self):
         status = "✓" if self.completed else "O"
-        return f"{status} {self.title}"
+        return f"{status} {self.title} ({self.priority})"
